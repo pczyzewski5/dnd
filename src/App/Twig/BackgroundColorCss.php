569df4807d94cfd7812073d940b9cead252054a3
pczyzewski <p.czyzewski5@gmail.com>
2023-09-28 21:38:13 +0200

    added param to BackgroundColorCss.php

--- src/App/Twig/BackgroundColorCss.php
+++ src/App/Twig/BackgroundColorCss.php
@@ -9,6 +9,13 @@ use Twig\TwigFunction;
 
 class BackgroundColorCss extends AbstractExtension
 {
+    private bool $devMode = false;
+
+    public function __construct(bool $devMode)
+    {
+        $this->devMode = $devMode;
+    }
+
     public function getFunctions(): array
     {
         return [
@@ -18,11 +25,17 @@ class BackgroundColorCss extends AbstractExtension
 
     public function generateCssRule(): string
     {
+        $opacity = 0;
+        if ($this->devMode) {
+            $opacity = 0.3;
+        }
+
         return \sprintf(
-          'background-color: rgba(%s, %s, %s, 0.3);',
+            'background-color: rgba(%s, %s, %s, %s);',
             \rand(0,255),
             \rand(0,255),
-            \rand(0,255)
+            \rand(0,255),
+            $opacity
         );
     }
 }
