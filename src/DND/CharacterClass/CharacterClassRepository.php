6c641eb2021eebc8d5b1ee10f965ddb51b83e970
pczyzewski <p.czyzewski5@gmail.com>
2023-10-12 20:34:35 +0200

    refactored character class repo

--- src/DND/CharacterClass/CharacterClassRepository.php
+++ src/DND/CharacterClass/CharacterClassRepository.php
@@ -2,46 +2,51 @@
 
 namespace DND\CharacterClass;
 
+use DND\Character\CharacterClassMapper;
 use DND\Domain\Enum\CharacterClassEnum;
 
 class CharacterClassRepository
 {
-    private const CHARACTER_DATA_FILEPATH = __DIR__ . '/character_class.json';
-    private const ARCHETYPE_DATA_FILEPATH = __DIR__ . '/character_archetype.json';
+    private const CHARACTER_DATA_FILEPATH = __DIR__ . '/class_data.json';
+    private const ARCHETYPE_DATA_FILEPATH = __DIR__ . '/archetype_data.json';
 
     public static function get(CharacterClassEnum $characterClassEnum): CharacterClass
     {
-        $archetypeData = self::findCharacterClassData($characterClassEnum, self::ARCHETYPE_DATA_FILEPATH);
-        if (null !== $archetypeData) {
-            $characterClassEnum = CharacterClassEnum::from($archetypeData['parent']);
-        }
-        $characterData = self::findCharacterClassData($characterClassEnum, self::CHARACTER_DATA_FILEPATH);
+        $archetypeData = CharacterClassMapper::isArchetype($characterClassEnum)
+            ? self::findCharacterClassData($characterClassEnum)
+            : null;
+
+        $characterData = self::findCharacterClassData(
+            CharacterClassMapper::isBaseClass($characterClassEnum)
+                ? $characterClassEnum
+                : CharacterClassMapper::getBaseClass($characterClassEnum)
+        );
 
         if (null === $characterData && null === $archetypeData) {
-            // @todo changeme
-            throw new \Exception('Class: ' . $characterClassEnum->getValue() . ', does not exists.');
+            throw new \Exception('Data for class: ' . $characterClassEnum->getValue() . ', does not exists.');
         }
 
         return new CharacterClass($characterClassEnum, $characterData, $archetypeData);
     }
 
-    private static function findCharacterClassData(
-        CharacterClassEnum|string $characterClass,
-        string $filepath
-    ): ?array {
-        if ($characterClass instanceof CharacterClassEnum) {
-            $characterClass = $characterClass->getValue();
-        }
+    private static function findCharacterClassData(CharacterClassEnum $characterClassEnum): ?array
+    {
+        $getDataFromFile = static function (string $filepath): array {
+            $data = \file_get_contents($filepath);
+            $data = \json_decode($data, true);
+            if (json_last_error() !== JSON_ERROR_NONE) {
+                // @todo changeme
+                throw new \Exception('Invalid json in: ' . $filepath);
+            }
 
-        $data = \file_get_contents($filepath);
-        $data = \json_decode($data, true);
-        if (json_last_error() !== JSON_ERROR_NONE) {
-            // @todo changeme
-            throw new \Exception('Invalid json in: ' . $filepath);
-        }
+            return $data;
+        };
 
-        return \array_key_exists($characterClass, $data)
-            ? $data[$characterClass]
-            : null;
+        $data = \array_merge(
+            $getDataFromFile(self::CHARACTER_DATA_FILEPATH),
+            $getDataFromFile(self::ARCHETYPE_DATA_FILEPATH)
+        );
+
+        return $data[$characterClassEnum->getValue()] ?? null;
     }
 }
