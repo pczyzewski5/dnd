c8c7c215cd41df57767135b9749ce0e7aee3a065
pczyzewski <p.czyzewski5@gmail.com>
2023-10-12 22:43:09 +0200

    character and character factory cleanup

--- src/DND/Character/Character.php
+++ src/DND/Character/Character.php
@@ -7,121 +7,72 @@ use DND\Calculators\DistanceCalculator;
 use DND\Calculators\HitDiceCalculator;
 use DND\Calculators\HitPointsCalculator;
 use DND\Calculators\InitiativeCalculator;
-use DND\Calculators\ProficiencyBonusCalculator;
 use DND\CharacterClass\CharacterClass;
-use DND\CharacterClass\CharacterClassHelper;
-use DND\CharacterClass\CharacterClassResolver;
 use DND\Domain\Ability\Abilities;
-use DND\Domain\Ability\AbilityMerger;
-use DND\Domain\AbilitySkills\AbilitySkillsFactory;
 use DND\Domain\Enum\AlignmentEnum;
 use DND\Domain\Enum\OriginEnum;
 use DND\Domain\Proficiency\Proficiencies;
 use DND\Domain\SavingThrows\SavingThrows;
-use DND\Domain\SavingThrows\SavingThrowsFactory;
 use DND\Domain\AbilitySkills\AbilitySkills;
 use DND\Race\Race;
 use DND\Skill\Skills;
 
 class Character
 {
-    private string $characterName;
-    private string $playerName;
-    private string $campaignName;
-    private Race $race;
-    private OriginEnum $origin;
-    private AlignmentEnum $alignment;
-    private Levels $levels;
-    private Abilities $abilities;
+    private CharacterClass $characterClass;
+    private AbilitySkills $abilitySkills;
     private Proficiencies $proficiencies;
-    private array $languages;
+    private SavingThrows $savingThrows;
+    private AlignmentEnum $alignment;
+    private Abilities $abilities;
+    private OriginEnum $origin;
+    private Skills $skills;
+    private Levels $levels;
+    private Race $race;
+    private string $characterName;
+    private string $campaignName;
+    private string $playerName;
     private array $resistances;
     private array $immunities;
+    private array $languages;
+    private ?CharacterClass $characterSubclass;
 
     public function __construct(
-        string $characterName,
-        string $playerName,
-        string $campaignName,
-        Race $race,
-        OriginEnum $origin,
-        AlignmentEnum $alignment,
-        Levels $levels,
-        Abilities $abilities,
+        CharacterClass $characterClass,
+        AbilitySkills $abilitySkills,
         Proficiencies $proficiencies,
-        array $languages,
+        SavingThrows $savingThrows,
+        AlignmentEnum $alignment,
+        Abilities $abilities,
+        OriginEnum $origin,
+        Skills $skills,
+        Levels $levels,
+        Race $race,
+        string $characterName,
+        string $campaignName,
+        string $playerName,
         array $resistances,
         array $immunities,
+        array $languages,
+        ?CharacterClass $characterSubclass
     ) {
-        $this->campaignName = $campaignName;
-        $this->characterName = $characterName;
-        $this->playerName = $playerName;
-        $this->race = $race;
-        $this->origin = $origin;
-        $this->alignment = $alignment;
-        $this->levels = $levels;
+        $this->characterClass = $characterClass;
+        $this->abilitySkills = $abilitySkills;
         $this->proficiencies = $proficiencies;
-        $this->languages = $languages;
+        $this->savingThrows = $savingThrows;
+        $this->alignment = $alignment;
+        $this->abilities = $abilities;
+        $this->origin = $origin;
+        $this->skills = $skills;
+        $this->levels = $levels;
+        $this->race = $race;
+        $this->characterName = $characterName;
+        $this->campaignName = $campaignName;
+        $this->playerName = $playerName;
         $this->resistances = $resistances;
         $this->immunities = $immunities;
-
-        $characterClassHelper = new CharacterClassHelper($levels);
-
-
-
-        $this->abilities = AbilityMerger::merge($abilities, $race);
-        $this->proficiencies->merge($characterClassHelper->getProficiencies());
-    }
-
-    public function getCampaignName(): string
-    {
-        return $this->campaignName;
-    }
-
-    public function getResistances(): array
-    {
-        return $this->resistances;
-    }
-
-    public function getImmunities(): array
-    {
-        return $this->immunities;
-    }
-
-    public function getProficiencies(): Proficiencies
-    {
-        return $this->proficiencies;
-    }
-
-    public function getLanguages(): array
-    {
-        return \array_unique(
-            \array_merge($this->race->getLanguages(), $this->languages)
-        );
-    }
-
-    public function getCharacterName(): string
-    {
-        return $this->characterName;
-    }
-
-    public function getCharacterClass(): CharacterClass
-    {
-        return CharacterClassResolver::getCharacterClass($this->levels);
-    }
-
-    public function getCharacterSubclass(): ?CharacterClass
-    {
-        return CharacterClassResolver::getCharacterSubclass($this->levels);
-    }
-
-    public function getPlayerName(): string
-    {
-        return $this->playerName;
-    }
-
-    public function getRace(): Race
-    {
-        return $this->race;
+        $this->languages = $languages;
+        $this->characterSubclass = $characterSubclass;
     }
 
     public function getSpeed(): float
@@ -144,50 +95,6 @@ class Character
         return ArmorClassCalculator::calculate($this->abilities);
     }
 
-    public function getOrigin(): OriginEnum
-    {
-        return $this->origin;
-    }
-
-    public function getAlignment(): AlignmentEnum
-    {
-        return $this->alignment;
-    }
-
-    public function getActualLevel(): int
-    {
-        return \count($this->levels->getLevels());
-    }
-
-    public function getAbilities(): Abilities
-    {
-        return $this->abilities;
-    }
-
-    public function getSavingThrows(): SavingThrows
-    {
-        return SavingThrowsFactory::create($this->abilities, $this->proficiencies, $this->getProficiencyBonus());
-    }
-
-    public function getAbilitySkills(): AbilitySkills
-    {
-        return AbilitySkillsFactory::create($this->abilities, $this->proficiencies, $this->getProficiencyBonus());
-    }
-
-    public function getSkills(): array
-    {
-        $skills = new Skills();
-        $skills->addSkills($this->race->getSkills());
-        $skills->addSkills($this->getCharacterClass()->getSkills());
-
-        return $skills->getSkills($this);
-    }
-
-    public function getProficiencyBonus(): int
-    {
-        return ProficiencyBonusCalculator::calculate($this->getActualLevel());
-    }
-
     public function getHitDices(): array
     {
         return HitDiceCalculator::calculate($this->levels);
@@ -196,5 +103,97 @@ class Character
     public function getHitPoints(): int
     {
         return HitPointsCalculator::calculate($this->abilities, $this->levels);
+    }
+
+    public function getCharacterClass(): CharacterClass
+    {
+        return $this->characterClass;
+    }
+
+    public function getAbilitySkills(): AbilitySkills
+    {
+        return $this->abilitySkills;
+    }
+
+    public function getProficiencies(): Proficiencies
+    {
+        return $this->proficiencies;
+    }
+
+    public function getSavingThrows(): SavingThrows
+    {
+        return $this->savingThrows;
+    }
+
+    public function getAlignment(): AlignmentEnum
+    {
+        return $this->alignment;
+    }
+
+    public function getAbilities(): Abilities
+    {
+        return $this->abilities;
+    }
+
+    public function getOrigin(): OriginEnum
+    {
+        return $this->origin;
+    }
+
+    public function getSkills(): array
+    {
+        return $this->skills->getSkills($this);
+    }
+
+    public function getRace(): Race
+    {
+        return $this->race;
+    }
+
+    public function getCharacterName(): string
+    {
+        return $this->characterName;
+    }
+
+    public function getCampaignName(): string
+    {
+        return $this->campaignName;
+    }
+
+    public function getPlayerName(): string
+    {
+        return $this->playerName;
+    }
+
+    public function getResistances(): array
+    {
+        return $this->resistances;
+    }
+
+    public function getImmunities(): array
+    {
+        return $this->immunities;
+    }
+
+    public function getLanguages(): array
+    {
+        return \array_unique(
+            \array_merge($this->race->getLanguages(), $this->languages)
+        );
+    }
+
+    public function getCharacterSubclass(): ?CharacterClass
+    {
+        return $this->characterSubclass ?? null;
+    }
+
+    public function getProficiencyBonus(): int
+    {
+        return $this->levels->getProficiencyBonus();
+    }
+
+    public function getActualLevel(): int
+    {
+        return \count($this->levels->getLevels());
     }
 }
--- src/DND/Character/CharacterFactory.php
+++ src/DND/Character/CharacterFactory.php
@@ -2,30 +2,55 @@
 
 namespace DND\Character;
 
+use DND\CharacterClass\CharacterClassResolver;
 use DND\Domain\Ability\AbilitiesFactory;
+use DND\Domain\Ability\AbilityMerger;
+use DND\Domain\AbilitySkills\AbilitySkillsFactory;
 use DND\Domain\Enum\AlignmentEnum;
 use DND\Domain\Enum\OriginEnum;
 use DND\Domain\Enum\RaceEnum;
 use DND\Domain\Proficiency\ProficienciesFactory;
+use DND\Domain\SavingThrows\SavingThrowsFactory;
 use DND\Race\RaceFactory;
+use DND\Skill\SkillsFactory;
 
 class CharacterFactory
 {
     public static function createFromArray(array $data): Character
     {
+        $levels = LevelsFactory::fromArray($data['levels']);
+        $characterClass = CharacterClassResolver::getCharacterClass($levels);
+        $proficiencies = \array_merge(
+            $data['proficiencies'],
+            $data['expert_proficiencies'],
+            $characterClass->getProficiencies()
+        );
+        $proficiencies = ProficienciesFactory::fromArray($proficiencies);
+        $characterSubclass = CharacterClassResolver::getCharacterSubclass($levels);
+        $race = RaceFactory::create(RaceEnum::from($data['race']));
+        $abilities = AbilitiesFactory::fromArray($data['starting_abilities']);
+        $abilities = AbilityMerger::merge($abilities, $race);
+        $abilitySkills = AbilitySkillsFactory::create($abilities, $proficiencies, $levels);
+        $skills = SkillsFactory::create($characterClass, $race);
+
         return new Character(
-            $data['character_name'],
-            $data['player_name'],
-            $data['campaign_name'],
-            RaceFactory::create(RaceEnum::from($data['race'])),
-            OriginEnum::from($data['origin']),
+            $characterClass,
+            $abilitySkills,
+            $proficiencies,
+            SavingThrowsFactory::create($abilities, $proficiencies, $levels),
             AlignmentEnum::from($data['alignment']),
-            LevelsFactory::fromArray($data['levels']),
-            AbilitiesFactory::fromArray($data['starting_abilities']),
-            ProficienciesFactory::fromArray($data['proficiencies'], $data['expert_proficiencies']),
+            $abilities,
+            OriginEnum::from($data['origin']),
+            $skills,
+            $levels,
+            $race,
+            $data['character_name'],
+            $data['campaign_name'],
+            $data['player_name'],
+            [],
+            [],
             $data['languages'],
-            [],
-            [],
+            $characterSubclass
         );
     }
 }
