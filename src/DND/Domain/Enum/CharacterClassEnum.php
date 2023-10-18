684f2f5e1308e88c268692669de2932aedd5f633
pczyzewski <p.czyzewski5@gmail.com>
2023-10-18 20:46:03 +0200

    cleaned up CharacterClassEnum.php

--- src/DND/Domain/Enum/CharacterClassEnum.php
+++ src/DND/Domain/Enum/CharacterClassEnum.php
@@ -9,16 +9,30 @@ class CharacterClassEnum extends Enum
     const BARBARIAN = 'barbarian';
     const BERSERKER = 'berserker';
 
-    const ROUGE = 'rouge';
-    const ASSASSIN = 'assassin';
+    const BARD = 'bard';
+
+    const CLERIC = 'cleric';
 
     const DRUID = 'druid';
     const CIRCLE_OF_MOON_DRUID = 'circle of moon druid';
-    const RANGER = 'ranger';
+
+    const FIGHTER = 'fighter';
+
+    const MONK = 'monk';
 
     const PALADIN = 'paladin';
     const OATH_OF_THE_ANCIENTS_PALADIN = 'oath of the ancients paladin';
 
+    const RANGER = 'ranger';
+
+    const ROUGE = 'rouge';
+
+    const ASSASSIN = 'assassin';
+
     const SORCERER = 'sorcerer';
     const WILD_MAGIC_SORCERER = 'wild magic sorcerer';
+
+    const WARLOCK = 'warlock';
+
+    const WIZARD = 'wizard';
 }
