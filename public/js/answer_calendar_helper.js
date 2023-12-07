823b6462d029bc63d0050ec4621932b22c6ad563
pczyzewski <p.czyzewski5@gmail.com>
2023-12-07 21:32:26 +0100

    added points refreshing at calendar answer

--- public/js/answer_calendar_helper.js
+++ public/js/answer_calendar_helper.js
@@ -7,31 +7,41 @@ class AnswerCalendarHelper {
         var $day = $('.can-vote');
         var $willAttendInput = $('#calendar_answer_form_will_attend');
         var $maybeAttendInput = $('#calendar_answer_form_maybe_attend')
-        var $willAttendDates =  '' == $willAttendInput.val()
+        var $willAttendDates =  '' === $willAttendInput.val()
             ? []
             : JSON.parse($willAttendInput.val());
-        var $maybeAttendDates = '' == $maybeAttendInput.val()
+        var $maybeAttendDates = '' === $maybeAttendInput.val()
             ? []
             : JSON.parse($maybeAttendInput.val());
 
         $day.on('click', function(e) {
-            var $day = $(e.target).attr('class') == 'can-vote'
+            var $day = $(e.target).attr('class') === 'can-vote'
                 ? $(e.target)
                 : $(e.target).parents('.can-vote');
             var $date = $day.find('.date').html();
+            var $pointsContainer = $('#' + $date + '_points');
+            var $actualPoints = parseInt($pointsContainer.html());
             var $icon = $day.find('.icon-container div');
+
+            console.log($pointsContainer, $actualPoints);
 
             if ($icon.hasClass('not-responded')) {
                 $icon.removeClass('not-responded');
                 $icon.addClass('will-attend');
 
                 $willAttendDates.push($date);
+                $pointsContainer.html(
+                    $actualPoints + 1
+                );
             } else if ($icon.hasClass('will-attend')) {
                 $icon.removeClass('will-attend');
                 $icon.addClass('maybe-attend');
 
                 $willAttendDates = removeFromArray($date, $willAttendDates);
                 $maybeAttendDates.push($date);
+                $pointsContainer.html(
+                    $actualPoints - 1
+                );
             } else if ($icon.hasClass('maybe-attend')) {
                 $icon.removeClass('maybe-attend');
                 $icon.addClass('not-responded');
