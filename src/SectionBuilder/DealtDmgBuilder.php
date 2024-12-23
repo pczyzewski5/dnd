457f94dd87f0fca516d9af0018cc8cb6eaa5a944
pczyzewski <pczyzewski@trans.eu>
2024-12-24 00:03:03 +0100

    added dealt dmg section

---
+++ src/SectionBuilder/DealtDmgBuilder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\SectionBuilder;
+
+class DealtDmgBuilder extends AbstractSectionBuilder
+{
+    public function build(bool $printMode = false): string
+    {
+        $context =  [];
+
+        return $this->twig->render(
+            'character_card/new_sections/dealt_dmg.html.twig',
+            $context
+        );
+    }
+}
