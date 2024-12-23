08346c9fc39605ce8939d4a4fe81745a7e89b94c
pczyzewski <pczyzewski@trans.eu>
2024-12-23 22:45:24 +0100

    Added race config

---
+++ src/Service/RaceService.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Entity\Race;
+use App\Race\RaceConfig;
+use App\Mapper\AbilityConfigDtoMapper;
+
+use function json_decode;
+use function json_last_error;
+
+class RaceService
+{
+    public function __construct(
+        private readonly AbilityConfigDtoMapper $abilityConfigDtoMapper
+    ) {
+
+    }
+
+    public function getRaceConfig(Race $race): RaceConfig
+    {
+       $config = $this->decodeJson($race->getConfig());
+
+        return new RaceConfig(
+            $this->getAsi($config)
+        );
+    }
+
+    private function decodeJson(string $json): array
+    {
+        $result = json_decode($json, true);
+
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            // @todo changeme
+            throw new \Exception('Invalid json in race config json');
+        }
+
+        return $result;
+    }
+
+    private function getAsi(array $config): array
+    {
+        return $this->abilityConfigDtoMapper->manyFromArray($config['asi'] ?? []);
+    }
+}
